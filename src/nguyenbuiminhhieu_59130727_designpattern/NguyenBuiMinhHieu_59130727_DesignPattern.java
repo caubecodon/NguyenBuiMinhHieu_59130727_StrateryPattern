2d23540f8af9dcc189665942475bafc978ba223c
Hieu <nguyenhiu99@gmail.com>
2020-04-16 20:43:20 +0700

    toang

---
+++ src/nguyenbuiminhhieu_59130727_designpattern/NguyenBuiMinhHieu_59130727_DesignPattern.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nguyenbuiminhhieu_59130727_designpattern;
+import Bai1.Context;
+
+/**
+ *
+ * @author Nguyen Bui Minh Hieu
+ */
+public class NguyenBuiMinhHieu_59130727_DesignPattern {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //bai1
+        System.out.println("Bài tập 1:");
+        Context.main(args);
+    }
+    
+}
